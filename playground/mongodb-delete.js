7f17709c9d498a1a01af6c64bcbe8aa9808eb30e
eladFrizi <okc.elad.35@gmail.com>
2017-08-04 20:31:22 +0300

    remove

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result => {
+    //     console.log(result);
+    // }))
+
+    //deleteOne
+    // db.collection('Todos').deleteOne({text: "Eat lunch"}).then(res => console.log(res))
+
+    //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then(res => {
+    //     console.log(res)
+    // })
+
+    // db.collection('Users').deleteMany({ name: 'Elad' }).then((result => {
+    //     console.log(result);
+    // }))
+
+    db.collection('Users').findOneAndDelete({ _id: new ObjectID("59849df82b38581e885fbcb5") }).then(res => {
+        console.log(res)
+    })
+
+
+    db.close();
+});
